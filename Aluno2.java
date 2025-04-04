f88341194bcb21cf9db5ac7750b01a665f1efcd4
Frazzon <161648912+Akumashii@users.noreply.github.com>
2025-04-03 23:32:29 -0300

    Create Aluno2.java

---
+++ Aluno2.java
@@ -0,0 +1,36 @@
+package aula20250328;
+
+public class Aluno {
+	public int matricula;
+	public String nome;
+	public String email;
+	
+	public Aluno(int matricula, String nome) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.email = this.gerarEmail();
+	}
+	
+	public String gerarEmail() {
+		String vetorNome[];
+		vetorNome = this.nome.toLowerCase().split(" ");
+		return vetorNome[0] + "." +vetorNome[vetorNome.length - 1] + "@ufn.edu.br";
+	}
+	
+	@Override
+	public String toString() {
+		return "Aluno [nome = " + nome + ", email= " + email + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Aluno aluno = (Aluno) obj;
+		if(this.email.equals(aluno.email)) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+}
